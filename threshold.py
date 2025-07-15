8b4875a1b69061067dda19269c9783fb77d867b4
radyaardinp <radya.ardinp@gmail.com>
2025-07-15 12:29:29 +0700

    Delete threshold.py

--- threshold.py
+++
@@ -1,7 +0,0 @@
-def calculate_thresholds(df):
-    return {
-        'daily_freq': df['daily_freq'].quantile(0.95),
-        'amount': df['amount'].quantile(0.95),
-        'failed_count': df['failed_count'].quantile(0.95),
-        'mismatch': df['mismatch'].quantile(0.95)
-    }
