06dfc06657b14ea1603f102d6dc2f3e793338b1b
radyaardinp <radya.ardinp@gmail.com>
2025-07-15 13:47:16 +0700

    Create normalize.py

---
+++ normalize.py
@@ -0,0 +1,17 @@
+# normalize.py
+import joblib
+import pandas as pd
+from selected_features import FEATURES
+
+def load_scaler(path='scaler.pkl'):
+    return joblib.load(path)
+
+def normalize_data(df, scaler=None):
+    if scaler is None:
+        scaler = load_scaler()
+    
+    # Ambil hanya fitur numerik yang sudah dipilih
+    data_to_scale = df[FEATURES].copy()
+    scaled = scaler.transform(data_to_scale)
+    
+    return pd.DataFrame(scaled, columns=FEATURES, index=df.index)
