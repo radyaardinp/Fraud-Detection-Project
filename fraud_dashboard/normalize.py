d97a20d7279b69726f4dc32a073e3cb5a6ebf1e3
radyaardinp <radya.ardinp@gmail.com>
2025-07-16 07:36:43 +0700

    Update normalize.py

--- fraud_dashboard/normalize.py
+++ fraud_dashboard/normalize.py
@@ -2,14 +2,15 @@
 import joblib
 import pandas as pd
 
+#Load FEATURES
+FEATURES = joblib.load("selected_features.joblib")
+
 def load_scaler(path='scaler.joblib'):
     return joblib.load(path)
 
 def normalize_data(df, scaler=None):
     if scaler is None:
         scaler = load_scaler()
-    if features is None:
-        raise ValueError("Fitur tidak diberikan ke normalize_data")
     
     # Ambil hanya fitur numerik yang sudah dipilih
     data_to_scale = df[features].copy()
