4494c58ec14ad9b7c8b9863498346a3916a0f1f4
radyaardinp <radya.ardinp@gmail.com>
2025-07-18 11:12:25 +0700

    Update dashboard4.py

--- fraud_dashboard/dashboard4.py
+++ fraud_dashboard/dashboard4.py
@@ -216,7 +216,7 @@ def page_upload():
             st.markdown("### 📊 Column Information")
             col_info = pd.DataFrame({
                 'Column': df.columns,
-                'Data Type': df.dtypes)
+                'Data Type': df.dtypes})
             # Buat keterangan manual untuk setiap kolom
             manual_descriptions = {
                 'id': 'Identitas unik transaksi',
