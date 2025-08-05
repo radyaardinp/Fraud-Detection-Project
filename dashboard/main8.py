7431ef471fec6c25378fcdcd6f5afc6e0fe00b1c
radyaardinp <radya.ardinp@gmail.com>
2025-08-05 22:55:42 +0700

    Update main8.py

--- dashboard/main8.py
+++ dashboard/main8.py
@@ -759,8 +759,8 @@ elif st.session_state.current_step == 2:
                 st.session_state.current_step = 3
                 st.rerun()
                 st.session_state.data = handle_missing_values(st.session_state.data)
-                    st.success("✅ Missing values berhasil ditangani!")
-                    st.rerun()
+                st.success("✅ Missing values berhasil ditangani!")
+                st.rerun()
 
 elif st.session_state.current_step == 3:
     # Step 3: Analysis
